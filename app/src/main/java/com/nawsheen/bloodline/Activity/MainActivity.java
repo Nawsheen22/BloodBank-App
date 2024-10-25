46ded56bfea9ee449b53cbc1b662863b015d9511
Nawsheen22 <nawsheensalsabeel18@gmail.com>
2024-10-26 01:01:58 +0600

    Splash Screen , Sign up , log in

---
+++ app/src/main/java/com/nawsheen/bloodline/Activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.nawsheen.bloodline.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nawsheen.bloodline.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/nawsheen/bloodline/Utils/Endpoints.java
@@ -0,0 +1,7 @@
+package com.nawsheen.bloodline.Utils;
+
+public class Endpoints {
+    private static final String base_url = "https://myfirst23.000webhostapp.com/";
+    public static final String register_url = base_url+"gotonew.php";
+    public static final String login_url = base_url+"login.php";
+}
